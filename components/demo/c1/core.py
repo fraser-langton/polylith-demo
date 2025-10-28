22f30579ac672472631f32be2cde149bdb034b07
Fraser Langton <fraser.langton@kogan.com.au>
2025-10-28 11:43:34 +1100

    demo

--- components/demo/c1/core.py
+++ components/demo/c1/core.py
@@ -1,3 +1,9 @@
+from pydantic import BaseModel
+
+class DemoC1(BaseModel):
+    name: str
+    age: int
+
 
 def get_demo_c1():
     return "demo_c1"
--- components/demo/c1/tests/test_api.py
+++ components/demo/c1/tests/test_api.py
@@ -1,4 +1,4 @@
-import httpx
+import requests
 
 from . import core
 
